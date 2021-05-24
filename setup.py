eac8929a11494fea720fb2939fa46c9e0b44068f
muhammed faris <faris.um2000@gmail.com>
2021-05-24 22:44:21 +0530

    readme file changes

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
   description = 'simple validator for input data (only for python dic)', 
   author = 'Muhammed Faris',               
   author_email = 'faris.um2000@gmail.com',      
-  url = 'https://github.com/faris404/reponame',   # Provide either the link to your github or to your website
+  url = 'https://github.com/faris404/make-sure',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['makesure', 'validation', 'parser','json validator'],   # Keywords that define your package best
   install_requires=[],
