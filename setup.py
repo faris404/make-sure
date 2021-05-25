a0020d9bda8a7b743a3a460874af9b227f224b31
muhammed faris <faris.um2000@gmail.com>
2021-05-25 13:37:17 +0530

    v0.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md','r') as f:
 setup(
   name = 'makesure',        
   packages = ['makesure'],   
-  version = '0.3',      
+  version = '0.4',      
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'simple validator for input data (only work for python dict)', 
   author = 'Muhammed Faris',               
